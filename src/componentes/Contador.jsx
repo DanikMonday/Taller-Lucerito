3fc3303f3497ab82a5954843aed0304199a5e20b
DanikMonday <daacostaf@unal.edu.co>
2023-01-04 23:26:33 -0500

    contador trash v1

--- src/componentes/Contador.jsx
+++ src/componentes/Contador.jsx
@@ -1,14 +1,23 @@
-import React, {useReducer, useState} from "react";
+import React, {useReducer} from "react";
+import { BsTrashFill} from "react-icons/bs";
 
 const initialState = {cantidad:0};
 
+const TYPES = {
+  INCREMENTO: "INCREMENTO",
+  DECREMENTO: "DECREMENTO",
+  RESET: "RESET"
+}
+// la funcion reducer recibe 2 parametros, state(estado anterior) y
+// action(contiene el tipo de accion)
 function reducer(state, action) {
   switch (action.type) {
-    case "incremento":
+    case "INCREMENTO":
       return {cantidad: state.cantidad + 1};
-    case "decremento":
+    case "DECREMENTO":
       return {cantidad: state.cantidad - 1}
     default:
+      // retorna el nuevo estado despues de ejecutar la funcion
       return state;
   }
   
@@ -19,9 +28,9 @@ function Contador(props){
  // El hook useReducer nos permite usar la funcion reducer 
 //  para manipular el estado de una variable
   const [state, dispatch] = useReducer(reducer, initialState)
- // dispatch es la funcion que actualiza el estado
-  const adicionar = () => dispatch({type:"incremento"});
-  const sustraer = () => dispatch({type: "decremento"});
+ // dispatch es la funcion que actualiza el estado, recibe el tipo de accion
+  const adicionar = () => dispatch({type: TYPES.INCREMENTO});
+  const sustraer = () => dispatch({type: TYPES.DECREMENTO});
 
   return(
    <div className="cont-conta">
@@ -29,6 +38,7 @@ function Contador(props){
           <p><button className="btn-sus" onClick={sustraer}>-</button></p>
           <p>{state.cantidad}</p>
           <p><button className="btn-adi" onClick={adicionar}>+</button></p>
+          <button className="btn-basu" > <BsTrashFill/></button>
          
           
    </div>
